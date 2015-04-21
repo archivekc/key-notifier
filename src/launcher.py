0b39ab24333156af2add02c8016a0d82e2124270
Jean-Daniel FISCHER <jean-daniel.fischer@keyconsulting.fr>
2015-04-21 10:26:21 +0200

    Improve logging.

--- src/launcher.py
+++ src/launcher.py
@@ -3,7 +3,13 @@ import logging
 from engine import SchedulingEngine
 
 if __name__ == '__main__':
-	logging.basicConfig(filename='keynotifier.log',level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+	logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+    handler = logging.handlers.RotatingFileHandler(filename='keynotifier.log', mode='a', maxBytes=10*1024*1024, backupCount=5)
+    formatter = logging.Formatter(fmt='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S')
+    handler.setFormatter(formatter)
+    handler.setLevel(logging.INFO)
+    logger.addHandler(handler)
 	# Init scheduling engine
 	notifierEngine = SchedulingEngine()
 
